6a2ae83f1b6e351f5c5d85cfa2a7732209380d66
Jihunz <thespringsong@gmail.com>
2022-04-22 00:07:38 +0900

    refactor: console.log() 및 불필요한 코드 삭제

--- drag_order_vanila.js
+++ drag_order_vanila.js
@@ -19,7 +19,6 @@ class DragOrder {
 
             // dragstart
             item.addEventListener("dragstart", e => {
-                console.log(`dragStart: ${e.target.dataset["order"]}`);
 
                 e.target.classList.add("dragStart");
 
@@ -27,44 +26,25 @@ class DragOrder {
             });
 
             // dragenter
-            item.addEventListener("dragenter", e => {
-                console.log(`dragenter: ${e.target.dataset["order"]}`);
-
-                e.target.classList.add("dragOver");
-            });
+            item.addEventListener("dragenter", e => e.target.classList.add("dragOver"));
 
             // dragleave
-            item.addEventListener("dragleave", e => {
-                console.log(`dragleave: ${e.target.dataset["order"]}`);
-
-                e.target.classList.remove("dragOver");
-            });
+            item.addEventListener("dragleave", e => e.target.classList.remove("dragOver"));
 
             // dragover
-            item.addEventListener("dragover", e => {
-                console.log(`dragover: ${e.target.dataset["order"]}`);
-
-                // 드래그가 끝났을 때 이벤트 버블링을 방지 -> event canceling
-                e.preventDefault();
-            });
+            item.addEventListener("dragover", e => e.preventDefault());
 
             // dragend
-            item.addEventListener("dragend", e => {
-                e.target.classList.remove("dragStart");
-            });
+            item.addEventListener("dragend", e => e.target.classList.remove("dragStart"));
 
             // drop
             item.addEventListener("drop", e => {
-                console.log(`drop: ${e.target.dataset["order"]}`);
-
                 e.preventDefault();
 
                 e.target.classList.remove("dragOver");
 
                 const source = e.dataTransfer.getData("text/plain");
                 const target = e.target.dataset["order"];
-
-                console.log(`MOVE: ${source} ===>>> ${target}`);
 
                 this.moveOrder(source, target);
             });
@@ -80,9 +60,6 @@ class DragOrder {
 
             if (source < target) {
                 if (order == source) {
-                    item.dataset["order"] = target;
-
-                    //구간 내에 있는 요소만 변동
                 } else if (order > source && order <= target)
                     item.dataset["order"] = order - 1;
             } else {
